9758deb8370d9f033d97dd9edb7256f0dee7dd20
Sven Büttner <s.buettner@friends-of-bytes.com>
2023-03-05 04:42:35 +0100

    State hinzugefügt Hier wird die Auswahl des Nutzers gespeichert

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,19 @@
 import './App.scss';
+import { useState } from 'react';
 import { Navbar } from '../Navbar/Navbar';
 
 export const App = () => {
+  
+  const [state, setState] = useState({
+    plantType: "",
+    winterHardinessZone: "",
+    careLevel: "",
+    exposition: "",
+    foliagePhase: "",
+    foliageColors: [],
+    flowerColors: [],
+  });
+
   return (  
     <div className="App">
       <Navbar />
