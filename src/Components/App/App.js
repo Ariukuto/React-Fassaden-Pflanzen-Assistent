d79a1423dd016e09e46f448729b5dde9769172e3
Sven Büttner <s.buettner@friends-of-bytes.com>
2023-03-05 17:44:17 +0100

    App Component refactoring - state einzelne state Objekte aufgeteilt - Layout der App erstellt - Componenten Navbar, Sidebar, Plantlist eingebunden - Layout Style für verschiedene Größen erstellt erstellt. Größe des Containers ist nun wichtig statt wie vorher die Bildschirmbreite

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,49 @@
 import './App.scss';
 import { useState } from 'react';
 import { Navbar } from '../Navbar/Navbar';
+import { Sidebar } from '../Sidebar/Sidebar';
+import { PlantList } from '../PlantList/PlantList';
 
 export const App = () => {
-  
-  const [state, setState] = useState({
-    plantType: "",
-    winterHardinessZone: "",
-    careLevel: "",
-    exposition: "",
-    foliagePhase: "",
-    foliageColors: [],
-    flowerColors: [],
-  });
+
+  const [plantType, setPlantType] = useState("");
+  const [winterHardinessZone, setWinterHardinessZone] = useState("");
+  const [careLevel, setCareLevel] = useState("");
+  const [exposition, setExposition] = useState("");
+  const [foliagePhase, setFoliagePhase] = useState("");
+  const [foliageColors, setFoliageColors] = useState([]);
+  const [flowerColors, setFlowerColors] = useState([]);
+
+  const state = {plantType, winterHardinessZone, careLevel, exposition, foliagePhase, foliageColors, flowerColors}
+
+  const stateUpdate = ({type, selected}) => {
+    if(type === "plantType")
+      setPlantType(selected ?? "")
+    if(type === "winterHardinessZone")
+      setWinterHardinessZone(selected ?? "")
+    if(type === "careLevel")
+      setCareLevel(selected ?? "")
+    if(type === "exposition")
+      setExposition(selected ?? "")
+    if(type === "foliagePhase")
+      setFoliagePhase(selected ?? "")
+    if(type === "foliageColors")
+      setFoliageColors(selected ?? [])
+    if(type === "flowerColors")
+      setFlowerColors(selected ?? [])
+  }
 
   return (  
-    <div className="App">
-      <Navbar />
+    <div className="App border">
+      <Navbar appState={state} onSelected={(v) => stateUpdate(v)} />
+      <div className='App-body'>
+        <div className='sidebar-container bg-light'>
+          <Sidebar appState={state} onSelected={(v) => stateUpdate(v)}/>
+        </div>
+        <div className='list-container'>
+          <PlantList appState={state}/>
+        </div>
+      </div>
     </div>
   );
 }
