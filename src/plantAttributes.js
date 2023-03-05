2b2e35aa7675124a044c6c5229350b86e340bdb3
Sven Büttner <s.buettner@friends-of-bytes.com>
2023-03-05 04:56:25 +0100

    Select Auswahlmöglichkeiten hinzugefügt

---
+++ src/plantAttributes.js
@@ -0,0 +1,50 @@
+export const PlantTypes = {
+	Moos: "Moos",
+  Staude: "Staude",
+  Gehoelz: "Gehölz",
+};
+
+export const Bluetenfarben = {
+  Gelb: "Gelb",
+  Gruen: "Grün",
+  Rosa: "Rosa",
+  Braun: "Braun",
+  Weiß: "Weiß",
+}
+
+export const Laubfarben = {
+  Gelb: "Gelb",
+  Gruen: "Grün",
+  Rosa: "Rosa",
+  Braun: "Braun",
+  Weiß: "Weiß",
+  Violett: "Violett",
+  Grau: "Grau",
+}
+
+export const Laubphasen = {
+  Sommergruen: "Sommergrün",
+  Immergruen: "Immergrün",
+  Wintergruen: "Wintergrün",
+}
+
+export const Pflegestufen = {
+  gering: "Gering",
+  Wenig: "Wenig",
+  Mittel: "Mittel",
+  Viel: "Viel"
+}
+
+export const Expositions = {
+  Sonnig: "Sonnig",
+  Halbschattig: "Halbschattig",
+  Schattig: "Schattig",
+}
+
+export const WinterHardinessZones = {
+  vier: "4",
+  fünf: "5",
+  sechs: "6",
+  sieben: "7",
+  acht: "8",
+}
